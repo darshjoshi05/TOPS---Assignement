04d30233091981179215bd917747ec679f3c391a
darshjoshi05 <81850161+darshjoshi05@users.noreply.github.com>
2024-06-17 17:30:20 +0530

    Add files via upload

---
+++ Module - 3 SE/3. Looping & Conditional Statements/8.max.c
@@ -0,0 +1,18 @@
+// 8. Write a program to find out the max from given number
+
+#include<stdio.h>
+int main() {
+	int n, rem, max;
+	printf("Enter a number: ");
+	scanf("%d", &n);
+	max = rem = n % 10;
+	while (n != 0) {
+		rem = n % 10;
+		n /= 10;
+		if (rem > max) {
+			max = rem;
+		}
+	}
+	printf("Max: %d", max);
+	return 0;
+}

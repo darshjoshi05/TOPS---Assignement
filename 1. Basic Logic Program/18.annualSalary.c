a994ef295a54c8a79bc03c75b9e9f8023b0a0ef5
darshjoshi05 <81850161+darshjoshi05@users.noreply.github.com>
2024-06-17 17:06:38 +0530

    Delete 1. Basic Logic Program directory

--- 1. Basic Logic Program/18.annualSalary.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
-	int mSalary, ySalary;
-	printf("Enter your monthly salary. ");
-	scanf("%d",&mSalary);
-	ySalary = mSalary * 12;
-	printf("Annual Salary: %d",ySalary);
-}
